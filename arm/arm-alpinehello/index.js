431b8efbd74acad728999546d218f841699e2390
Marcello de Sales <marcello.desales@gmail.com>
2016-11-02 08:39:26 -0700

    Update index.js, showing the container ID

--- arm/arm-alpinehello/index.js
+++ arm/arm-alpinehello/index.js
@@ -5,10 +5,10 @@ let app = express();
 
 app.get("/", (req, res) =>
 {
-   res.status(200).send("Hello");
+   res.status(200).send("Hello... I am " + process.env.HOSTNAME);
 });
 
 app.listen(3000, () => {
-   console.log("I'm listening.")
+   console.log("I'm listening from " + process.env.ENV_VARIABLE)
 });
 
